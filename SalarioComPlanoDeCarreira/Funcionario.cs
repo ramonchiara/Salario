cddb48020349b9c9908ff98bee7db997646f8f56
Ramon Chiara <ramonchiara@gmail.com>
2016-09-05 22:31:23 -0300

    Solução do Exercício.

---
+++ SalarioComPlanoDeCarreira/Funcionario.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioComPlanoDeCarreira
+{
+    class Funcionario
+    {
+        private string Nome;
+        private int Nivel;
+        private double Horas;
+
+        public Funcionario(string nome, int nivel, double horas)
+        {
+            this.Nome = nome;
+            this.Nivel = nivel;
+            this.Horas = horas;
+        }
+
+        public decimal GetSalario()
+        {
+            decimal valor = GetValor();
+            return valor * Convert.ToDecimal(Horas);
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public double GetHoras()
+        {
+            return Horas;
+        }
+
+        public decimal GetValor()
+        {
+            decimal valor = 0;
+
+            switch (Nivel)
+            {
+                case 1: valor = 15; break;
+                case 2: valor = 18; break;
+                case 3: valor = 22; break;
+                case 4: valor = 27; break;
+                case 5: valor = 33; break;
+            }
+
+            return valor;
+        }
+
+        public int GetNivel()
+        {
+            return Nivel;
+        }
+
+
+    }
+}
---
+++ SalarioComPlanoDeCarreira/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioComPlanoDeCarreira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entradas
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Nível: ");
+            int nivel = int.Parse(Console.ReadLine());
+            Console.Write("Horas: ");
+            double horas = double.Parse(Console.ReadLine());
+
+            // Processamento
+            Funcionario funcionario = new Funcionario(nome, nivel, horas);
+            decimal salario = funcionario.GetSalario();
+
+            // Saída
+            Console.WriteLine("O funcionário {0} trabalhou {1} horas por R$ {2} (Nível {3}) e deve receber R$ {4}",
+                funcionario.GetNome(), funcionario.GetHoras(), funcionario.GetValor(), funcionario.GetNivel(), salario);
+        }
+    }
+}

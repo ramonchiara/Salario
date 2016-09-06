22a750507ac126d98854079e927fb77fa6c1de2a
Ramon Chiara <ramonchiara@gmail.com>
2016-09-05 22:40:04 -0300

    Solução do Exercício 1

---
+++ SalarioPorHora/Funcionario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioPorHora
+{
+    class Funcionario
+    {
+        private string Nome;
+        private decimal Valor;
+        private double Horas;
+
+        public Funcionario(string nome, decimal valor, double horas)
+        {
+            this.Nome = nome;
+            this.Valor = valor;
+            this.Horas = horas;
+        }
+
+        public decimal GetSalario()
+        {
+            return Valor * Convert.ToDecimal(Horas);
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public double GetHoras()
+        {
+            return Horas;
+        }
+
+        public decimal GetValor()
+        {
+            return Valor;
+        }
+    }
+}
---
+++ SalarioPorHora/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioPorHora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entradas
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Valor: ");
+            decimal valor = int.Parse(Console.ReadLine());
+            Console.Write("Horas: ");
+            double horas = double.Parse(Console.ReadLine());
+
+            // Processamento
+            Funcionario funcionario = new Funcionario(nome, valor, horas);
+            decimal salario = funcionario.GetSalario();
+
+            // Saída
+            Console.WriteLine("O funcionário {0} trabalhou {1} horas por R$ {2} e deve receber R$ {3}",
+                funcionario.GetNome(), funcionario.GetHoras(), funcionario.GetValor(), salario);
+        }
+    }
+}

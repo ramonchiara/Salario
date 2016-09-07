b9d098620f820fa37a52f33e9b16728d832ab5f5
Ramon Chiara <ramonchiara@gmail.com>
2016-09-07 00:46:30 -0300

    Solução do Exercício 3

--- SalarioComPlanoDeCarreiraPromocao/Program.cs
+++ SalarioComPlanoDeCarreiraPromocao/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SalarioComPlanoDeCarreiraPromocao
 {
-    class MainClass
+    class Program
     {
-        public static void Main (string [] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine ("Hello World!");
+            // Entradas
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Nível: ");
+            int nivel = int.Parse(Console.ReadLine());
+            Console.Write("Horas: ");
+            double horas = double.Parse(Console.ReadLine());
+            Console.Write("Tem promoção: ");
+            string promocao = Console.ReadLine();
+
+            // Processamento
+            Funcionario funcionario = new Funcionario(nome, nivel, horas);
+            decimal salario = funcionario.GetSalario();
+
+            // Saída
+            Console.WriteLine("O funcionário {0} trabalhou {1} horas por R$ {2} (Nível {3}) e deve receber R$ {4}",
+                funcionario.GetNome(), funcionario.GetHoras(), funcionario.GetValor(), funcionario.GetNivel(), salario);
+
+            if (promocao == "s")
+            {
+                funcionario.Promover();
+                salario = funcionario.GetSalario();
+
+                Console.WriteLine("O funcionário {0} teve promoção, trabalhou {1} horas por R$ {2} (Nível {3}) e o seu novo salário é de R$ {4}",
+                    funcionario.GetNome(), funcionario.GetHoras(), funcionario.GetValor(), funcionario.GetNivel(), salario);
+            }
         }
     }
 }
